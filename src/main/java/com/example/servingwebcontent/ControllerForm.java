d560962cb52322be9bd2369a7534ea964ee64b92
gfernandez09 <47225340+gfernandez09@users.noreply.github.com>
2021-04-16 19:21:59 +0200

    Done

---
+++ src/main/java/com/example/servingwebcontent/ControllerForm.java
@@ -0,0 +1,13 @@
+package com.example.servingwebcontent;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ControllerForm {
+    @PostMapping("/formulariospring")
+    public String form(String personName,String personAddress){
+        return "formulariospring";
+    }
+}

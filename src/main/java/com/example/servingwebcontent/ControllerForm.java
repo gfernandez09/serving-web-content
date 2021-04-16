23e609ee60f4737e870672ccedb313887b50d780
gfernandez09 <47225340+gfernandez09@users.noreply.github.com>
2021-04-16 20:32:30 +0200

    Done

--- src/main/java/com/example/servingwebcontent/ControllerForm.java
+++ src/main/java/com/example/servingwebcontent/ControllerForm.java
@@ -1,13 +1,20 @@
 package com.example.servingwebcontent;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ControllerForm {
     @PostMapping("/formulariospring")
-    public String form(String personName,String contrasenya){
+    public String form(String personName,String contrasenya,Model model){
+        model.addAttribute("name", personName);
+        model.addAttribute("contrasenya", contrasenya);
+        return "resultadoPeticiones";
+    }
+    @GetMapping("/formulariospring")
+    public String postForm(String personName,String contrasenya,Model model){
         return "formulariospring";
     }
 }
